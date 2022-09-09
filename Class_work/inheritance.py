5a29c8d37917b83696c77a34c852ca9d845b1d80
IbrahimGanaMalgwi <ibrahimganamalgwi@outlook.com>
2022-09-09 15:06:37 +0100

    Codes updates added

--- Class_work/inheritance.py
+++ Class_work/inheritance.py
@@ -13,21 +13,33 @@ class Human:
 
 
 class Manager(Human):
+    def __init__(self, first_name, last_name, age, sex, bonus: float):
+        self.bonus = bonus
+        super().__init__(first_name, last_name, age, sex)
+
     def full_name(self):
         return f"{self.last_name[0].upper()}. {self.first_name}"
 
+    def pay_salary(self, salary):
+        return salary + self.bonus
+
 
 class Employee(Human):
-    pass
+    def pay_salary(self, salary):
+        return salary
 
 
 employee1 = Employee("Judith", "Malgwi", 4, "Female")
 print(employee1.full_name())
 print(employee1)
 
-manager1: Manager = Manager("Joanna", "Malgwi", 1, "Female")
+manager1: Manager = Manager("Joanna", "Malgwi", 1, "Female", 1200)
 print(manager1.full_name())
 
 human_list = [employee1, manager1, Human("Sapien", "Homo", 0, "Unknown")]
 for human in human_list:
     print(human.full_name())
+
+workers_list = [employee1, manager1]
+for human in human_list:
+    print(human.pay_salary(50_000))
